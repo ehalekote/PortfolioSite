53707511efcdba8f1cb24bedc67ae1d0446a873f
Eshan Halekote <ehalekote@Eshans-MBP-3.attlocal.net>
2018-10-20 20:59:24 -0500

    updated image sizes

--- src/components/layout/Gallery.js
+++ src/components/layout/Gallery.js
@@ -19,7 +19,7 @@ class Gallery extends React.Component {
             coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/Aeroponics.jpg?raw=true"
           />
           <GalleryCard
-            coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/TabbyTax.png?raw=true"
+            coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/SmartRock.jpg?raw=true"
           />
           <GalleryCard
             coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/Nara.jpg?raw=true"
@@ -30,12 +30,20 @@ class Gallery extends React.Component {
             coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/SeaCow.jpg?raw=true"
           />
           <GalleryCard
-            coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/SmartRock.jpg?raw=true"
+            coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/balur.png?raw=true"
           />
         </div>
         <div id="galleryCol">
           <GalleryCard
             coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/Armada.png?raw=true"
+          />
+          <GalleryCard
+            coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/Nara.jpg?raw=true"
+          />
+        </div>
+        <div id="galleryCol">
+          <GalleryCard
+            coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/TabbyTax.png?raw=true"
           />
           <GalleryCard
             coverPicture="https://github.com/ehalekote/PortfolioSite/blob/master/src/images/balur.png?raw=true"
